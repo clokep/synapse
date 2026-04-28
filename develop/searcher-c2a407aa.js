cb60573469530f6c09b182e3ffae9e657f160d3a
clokep <clokep@users.noreply.github.com>
2026-04-28 13:36:15 +0000

    deploy: 449b1a4234b3868da3bbcf71714e782f0c37360e

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e2b11c99.js',
+                path_to_root + 'searchindex-c6fa9dbe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
